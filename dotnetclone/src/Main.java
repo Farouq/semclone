679a8929f8b8be69a0856c8864fedb68de711d89
ikeivanloo <iman@deepstar.encs.private>
2012-02-22 20:43:04 -0500

    init

---
+++ dotnetclone/src/Main.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		runCommandWithOutput("your command", "text file address");
+	}
+	
+	private static void runCommandWithOutput(String command, String outputFile) throws Exception
+	{
+		ArrayList<String> output=new ArrayList<String>();
+		 Runtime rt = Runtime.getRuntime();
+         Process pr = rt.exec(command);
+
+         BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+
+         String line=null;
+
+         while((line=input.readLine()) != null) {
+            // System.out.println(line);
+             output.add(line);
+         }
+
+         int exitVal = pr.waitFor();
+        /* if(exitVal!=0)
+         {
+         System.out.println("Exited with error code "+exitVal +"    command: "+command);
+         }*/
+         writeToFile(output, outputFile);
+	}
+	
+	private static void writeToFile(ArrayList<String> lines,String fileAddress) throws Exception{
+		  BufferedWriter bufferedWriter = null;
+			bufferedWriter = new BufferedWriter(new FileWriter(fileAddress));
+		 
+			for(String s : lines)
+			{
+				
+		 
+			    bufferedWriter.write(s);
+			  bufferedWriter.newLine();
+			}
+			 
+			  bufferedWriter.flush();
+			  bufferedWriter.close();  
+	}
+
+}
