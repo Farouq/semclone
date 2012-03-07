82068889689499b1fcf7c043540dccb77c462687
ikeivanloo <imanmahsa@gmail.com>
2012-03-06 19:53:07 -0500

    The Binary parser is done. It exports both binary and source code into two XML files.

--- dotnetclone/src/publisher/StarterPublisher.java
+++ dotnetclone/src/publisher/StarterPublisher.java
@@ -7,8 +7,6 @@ import java.io.FileWriter;
  
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
-
 import configuration.Configuration;
  
 
@@ -40,6 +38,8 @@ public class StarterPublisher {
 		System.out.println("step3");
 		step3_writeDisassembleContentToFiles(config,files,disassembledCodeList,false);
 		
+		System.out.println("step4");
+		step4_exportToXML(config,files,disassembledCodeList);
 	 
  
 		System.out.println("ver plusma");
@@ -68,6 +68,7 @@ public class StarterPublisher {
         	ArrayList<String> disassebledCode=new ArrayList<String>();
             Runtime rt = Runtime.getRuntime();
             String command=config.disassembeler_EXE_path+"   "+file + "  /source /text ";
+           // String command=config.disassembeler_EXE_path+"   "+file + "  /source /text /linenum ";
          
              Process pr = rt.exec(command);
              BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
@@ -128,6 +129,147 @@ public class StarterPublisher {
 			}
 	}
 
-
-
+	private static void step4_exportToXML(Configuration config,ArrayList<String> files,ArrayList<ArrayList<String>> disassembledCodeList) throws Exception
+	{
+		String currentSourceFileAddress="NULL";
+		
+		for(int i=0;i< files.size();i++)	
+		{
+			
+			ArrayList<String> out_lines_binary=new ArrayList<String>();
+			ArrayList<String> out_lines_source=new ArrayList<String>();
+			ArrayList<String> in_lines=disassembledCodeList.get(i);
+			out_lines_binary.add("<project><name></name><description></description><prog_language></prog_language><source_elements>");
+			out_lines_source.add("<project><name></name><description></description><prog_language></prog_language><source_elements>");
+			
+			ArrayList<String> methodBlockBuffer_binary=new ArrayList<String>();
+			ArrayList<String> methodBlockBuffer_source=new ArrayList<String>();
+			boolean bInsideMethodBlock=false;
+			int methoBlockStartLine=Integer.MAX_VALUE;
+			int methoBlockEndLine=Integer.MAX_VALUE;
+			for(int j=0;j<in_lines.size();j++)
+			{
+				if(in_lines.get(j).startsWith("// Source File '"))
+				{
+					String t=in_lines.get(j).replace("// Source File '", "");
+					t=t.substring(0,t.lastIndexOf("'"));
+					currentSourceFileAddress=t;
+				}
+				
+				if(in_lines.get(j).startsWith("  .method"))
+				{
+					while(!in_lines.get(j).trim().equals("{"))
+					{
+						j++;
+					}
+					bInsideMethodBlock=true;
+					methoBlockStartLine=Integer.MAX_VALUE;
+					methoBlockEndLine=Integer.MAX_VALUE;
+				}
+				
+				if(in_lines.get(j).startsWith("//"))
+				{
+					try
+					{
+					String[] cols= in_lines.get(j).split(":");
+					int ln=Integer.parseInt(cols[0].replace("//",""));
+					
+					if(bInsideMethodBlock)
+					{
+						if(methoBlockStartLine==Integer.MAX_VALUE)
+						{
+							methoBlockStartLine=ln;
+							
+						}
+						methoBlockEndLine=ln;
+					}
+					}
+					catch(Exception ex)
+					{
+						
+					}
+				}
+				
+				if(bInsideMethodBlock && !(in_lines.get(j).startsWith("//")  || in_lines.get(j).trim().startsWith("} // end of method")) )
+				{
+					methodBlockBuffer_binary.add(in_lines.get(j));
+				}
+				
+				if(bInsideMethodBlock && in_lines.get(j).startsWith("//")  )
+				{
+					
+					try
+					{
+					String[] cols= in_lines.get(j).split(":");
+					int ln=Integer.parseInt(cols[0].replace("//",""));
+					
+					methodBlockBuffer_source.add(cols[1]);
+					}
+					catch(Exception ex)
+					{
+						
+					}
+				
+					
+					
+				}
+				
+				if(bInsideMethodBlock && in_lines.get(j).trim().startsWith("} // end of method") )
+				{
+					out_lines_binary.add("<source file=\""+currentSourceFileAddress+"\" startline=\""+methoBlockStartLine+"\" endline=\""+methoBlockEndLine+"\"><![CDATA[");
+					out_lines_source.add("<source file=\""+currentSourceFileAddress+"\" startline=\""+methoBlockStartLine+"\" endline=\""+methoBlockEndLine+"\"><![CDATA[");
+					for(String llll : methodBlockBuffer_binary)
+					{
+						out_lines_binary.add(llll);
+					}
+					for(String llll : methodBlockBuffer_source)
+					{
+						out_lines_source.add(llll);
+					}
+					out_lines_binary.add("]]></source>");
+					out_lines_source.add("]]></source>");
+					methodBlockBuffer_binary.clear();
+					methodBlockBuffer_source.clear();
+					bInsideMethodBlock=false;
+					methoBlockStartLine=Integer.MAX_VALUE;
+					methoBlockEndLine=Integer.MAX_VALUE;
+				}
+				
+				
+				
+			}
+			
+			
+			out_lines_binary.add("</source_elements></project>");
+			out_lines_source.add("</source_elements></project>");
+			
+			writeToXMLFile(config,files.get(i),i,"binary",out_lines_binary);
+			writeToXMLFile(config,files.get(i),i,"source",out_lines_source);
+		}
+		
+	}
+	private static void writeToXMLFile(Configuration config,String filename,int fileID,String format,ArrayList<String> lines) throws Exception
+	{
+	  
+			 
+				String outputFileAddress=config.disassebledAddress+"/"+"XXXXXXXXXXXXXXXXXXXXX";
+			 
+				File f=new File(filename);
+				outputFileAddress=config.disassebledAddress+"/"+f.getName()+"_"+fileID+"_"+format+".xml";
+				 
+				
+			BufferedWriter bufferedWriter = null;
+			bufferedWriter = new BufferedWriter(new FileWriter(outputFileAddress));
+			 
+			  for(String line :lines)
+			 {
+				  //System.out.println(line);
+				  bufferedWriter.write(line);
+				  bufferedWriter.newLine();
+			 }
+			  bufferedWriter.flush();
+			  bufferedWriter.close();
+			  
+		 
+	}
 }
